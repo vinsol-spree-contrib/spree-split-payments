1d8ef0453d3c95db755fbede33646df3a8cfaa2e
manish kangia <manishkangia@manishs-MacBook-Pro.local>
2014-03-19 14:59:15 +0530

    add javascript for better user interface to select partial methods

--- app/assets/javascripts/store/spree-split-payments.js
+++ app/assets/javascripts/store/spree-split-payments.js
@@ -1,1 +1,24 @@
 //= require store/spree_frontend
+$(document).ready(function() {
+  amountDivs = $("#split-payments-data div");
+  if(amountDivs.length > 0) {
+    for(i = 0; i < amountDivs.length; i++) {
+      $("label.pm-amount[data-pm-id='"+ amountDivs[i].getAttribute('data-pm-id') +"']").html(amountDivs[i].getAttribute('data-pm-amount'));
+    }
+  }
+
+  $("input[name='order[split_payments][][payment_method_id]']").click(function() {
+    selected_partial_methods = $("input[name='order[split_payments][][payment_method_id]']:checked");
+    partial_payment_total = 0;
+    if(selected_partial_methods.length > 0) {
+      for(i = 0; i < selected_partial_methods.length; i++) {
+        partial_payment_total += parseInt($("#split-payments-data div[data-pm-id='"+ $(this).val() +"']")[0].getAttribute('data-pm-amount'));
+      }
+    }
+    if(partial_payment_total >= order_balance ) {
+      $("input[name='order[split_payments][][payment_method_id]']:unchecked").attr("disabled", "disabled");
+    } else {
+      $("input[name='order[split_payments][][payment_method_id]']").attr("disabled", false);
+    }
+  });
+});
